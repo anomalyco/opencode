9910f4047ac466f4642295e0764bd8e0d805ef5f
Brendan Allan <brendonovich@outlook.com>
2025-11-22 19:51:54 +0800

    update enterprise too

---
+++ packages/enterprise/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, PluginOption} from "vite"
+import { solidStart } from "@solidjs/start/config"
+import { nitro } from 'nitro/vite'
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    solidStart() as PluginOption,
+    nitro()
+  ],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: true,
+  },
+})
