9d28f9ef8d76831d2f43db2ccba5dba5e28a9bcc
GitHub Action <action@github.com>
2025-11-22 11:56:53 +0000

    chore: format code

--- packages/enterprise/vite.config.ts
+++ packages/enterprise/vite.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig, PluginOption} from "vite"
+import { defineConfig, PluginOption } from "vite"
 import { solidStart } from "@solidjs/start/config"
-import { nitro } from 'nitro/vite'
+import { nitro } from "nitro/vite"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    solidStart() as PluginOption,
-    nitro()
-  ],
+  plugins: [tailwindcss(), solidStart() as PluginOption, nitro()],
   server: {
     host: "0.0.0.0",
     allowedHosts: true,
