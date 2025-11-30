4fc556821d796cf99c375c94fdd9cece5e6680f8
Ariane Emory <ariane.emory@protonmail.com>
2025-11-30 02:07:07 -0500

    style: nit-picky fiddling with whitespace

--- packages/sdk/go/config.go
+++ packages/sdk/go/config.go
@@ -1834,12 +1834,12 @@ func (r ConfigShare) IsKnown() bool {
 // TUI specific settings
 type ConfigTui struct {
 	// TUI scroll speed
-	ScrollSpeed float64     `json:"scroll_speed"`
+	ScrollSpeed  float64       `json:"scroll_speed"`
 	// Enable copying text to clipboard when selected with mouse
-	CopyOnSelect bool       `json:"copy_on_select"`
+	CopyOnSelect bool          `json:"copy_on_select"`
 	// Control diff rendering style: 'auto' adapts to terminal width, 'stacked' always shows single column
-	DiffStyle string        `json:"diff_style"`
-	JSON      configTuiJSON `json:"-"`
+	DiffStyle    string        `json:"diff_style"`
+	JSON         configTuiJSON `json:"-"`
 }
 
 // configTuiJSON contains the JSON metadata for the struct [ConfigTui]
