4b1343bdf67a4c9dcd5f7e963aa284f49b7081d5
Brendan Allan <brendonovich@outlook.com>
2025-12-04 13:22:50 +0800

    OpenCode Desktop

---
+++ packages/desktop/vite.js
@@ -0,0 +1,24 @@
+import solidPlugin from "vite-plugin-solid"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
+import { fileURLToPath } from "url"
+
+/**
+ * @type {import("vite").PluginOption}
+ */
+export default [
+  {
+    name: "opencode-desktop:config",
+    config() {
+      return {
+        resolve: {
+          alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+          },
+        },
+      }
+    },
+  },
+  tailwindcss(),
+  solidPlugin(),
+]
