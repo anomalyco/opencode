0e94b4e5aa647854465c8083e01d227b7df8930b
Brendan Allan <brendonovich@outlook.com>
2025-12-04 15:47:59 +0800

    use desktop vite plugin

--- packages/desktop/vite.config.ts
+++ packages/desktop/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite"
-import solidPlugin from "vite-plugin-solid"
-import tailwindcss from "@tailwindcss/vite"
 import desktopPlugin from "./vite"
 
 export default defineConfig({
-  plugins: [desktopPlugin, tailwindcss(), solidPlugin()] as any,
+  plugins: [desktopPlugin] as any,
   server: {
     host: "0.0.0.0",
     allowedHosts: true,
