bea2d2af18381e199b46ce2829b1f1e70efdc809
bishalr0y <bishalroy895@gmail.com>
2025-12-08 18:08:24 +0530

    revert: catppuccin theme

--- packages/opencode/src/cli/cmd/tui/context/theme.tsx
+++ packages/opencode/src/cli/cmd/tui/context/theme.tsx
@@ -5,7 +5,7 @@ import { useSync } from "@tui/context/sync"
 import { createSimpleContext } from "./helper"
 import aura from "./theme/aura.json" with { type: "json" }
 import ayu from "./theme/ayu.json" with { type: "json" }
-import catppuccinMocha from "./theme/catppuccin-mocha.json" with { type: "json" }
+import catppuccin from "./theme/catppuccin.json" with { type: "json" }
 import catppuccinMacchiato from "./theme/catppuccin-macchiato.json" with { type: "json" }
 import cobalt2 from "./theme/cobalt2.json" with { type: "json" }
 import dracula from "./theme/dracula.json" with { type: "json" }
@@ -131,7 +131,7 @@ type ThemeJson = {
 export const DEFAULT_THEMES: Record<string, ThemeJson> = {
   aura,
   ayu,
-  ["catppuccin-mocha"]: catppuccinMocha,
+  catppuccin,
   ["catppuccin-macchiato"]: catppuccinMacchiato,
   cobalt2,
   dracula,
