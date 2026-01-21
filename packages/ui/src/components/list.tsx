95189b021dfee2f64dd860c57373cc589545d355
Devin Griffin <dgriffin@wustl.edu>
2026-01-20 22:27:17 -0600

    Use more readable clear-search icon

--- packages/ui/src/components/list.tsx
+++ packages/ui/src/components/list.tsx
@@ -219,7 +219,7 @@ export function List<T>(props: ListProps<T> & { ref?: (ref: ListRef) => void }) 
             />
           </div>
           <Show when={internalFilter()}>
-            <IconButton icon="circle-x" variant="ghost" onClick={() => setInternalFilter("")} />
+            <IconButton icon="close" variant="ghost" onClick={() => setInternalFilter("")} />
           </Show>
         </div>
       </Show>
