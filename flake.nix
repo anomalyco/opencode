3469bc7d4e5a16af7614288bddb2e5f137a7c51b
MBanucu <michael.banucu@googlemail.com>
2026-02-12 09:57:48 +0100

    core: switch to nixpkgs-unstable-small for more frequent package updates and faster builds

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "OpenCode development flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable-small";
   };
 
   outputs =
