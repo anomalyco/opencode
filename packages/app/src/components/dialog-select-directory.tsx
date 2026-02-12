5118b9e5a38d0616880af66a9bf4b93d7372d05f
Pablo P Varela <pablo@pablopunk.com>
2026-02-12 13:29:47 +0100

    missing translation key

--- packages/app/src/components/dialog-select-directory.tsx
+++ packages/app/src/components/dialog-select-directory.tsx
@@ -334,7 +334,7 @@ export function DialogSelectDirectory(props: DialogSelectDirectoryProps) {
       <div class="flex flex-col">
         <Show when={recentProjects().length > 0}>
           <div class="px-3 pb-2 mb-2">
-            <div class="text-12-regular text-text-weak mb-2 ml-2">Recent Projects</div>
+            <div class="text-12-regular text-text-weak mb-2 ml-2">{language.t("home.recentProjects")}</div>
             <div class="space-y-1">
               {recentProjects().map((project) => (
                 <RecentProjectItem project={project} onSelect={resolve} homedir={sync.data.path.home} />
