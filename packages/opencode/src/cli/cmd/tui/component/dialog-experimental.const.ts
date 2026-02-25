6a3c65298c20df5c1941428000bc34ba29752cc5
Arav Jain <aravhawk@gmail.com>
2026-02-25 17:53:15 -0600

    Address PR feedback for experimental flags dialog

---
+++ packages/opencode/src/cli/cmd/tui/component/dialog-experimental.const.ts
@@ -0,0 +1,5 @@
+export const ENV_NUMBER = /^[1-9]\d*$/
+export const ENV_LINE = /\r?\n/
+export const ENV_EXPORT = /^\s*export\s+/
+export const ENV_KEY = /^\s*(?:export\s+)?([^=\s]+)\s*=/
+export const ENV_LABEL = /^OPENCODE_/
