f7fc133e61d133eb070be29cb1060a8f0f446e61
Prax Lannister <prakhar.2019ca66@gmail.com>
2026-02-28 18:48:57 +0530

    fix: make loading screen icon much bigger (280px, opacity 30%)

--- packages/desktop/src/loading.tsx
+++ packages/desktop/src/loading.tsx
@@ -64,7 +64,7 @@ render(() => {
       <div class="w-screen h-screen bg-background-base flex items-center justify-center">
         <Font />
         <div class="flex flex-col items-center gap-11">
-          <Splash class="w-40 h-40 opacity-20" />
+          <Splash class="w-[280px] h-[280px] opacity-30" />
           <div class="w-60 flex flex-col items-center gap-4" aria-live="polite">
             <span class="w-full overflow-hidden text-center text-ellipsis whitespace-nowrap text-text-strong text-14-normal">
               {status()}
