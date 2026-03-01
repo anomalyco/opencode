34d161bc86e3f6cb0b7545d2591265242c2c76b2
vaur94 <100377859+vaur94@users.noreply.github.com>
2026-03-01 05:09:58 +0300

    chore: revert plugin settings UI from console

--- packages/console/app/src/routes/workspace/[id]/settings/index.tsx
+++ packages/console/app/src/routes/workspace/[id]/settings/index.tsx
@@ -1,23 +1,10 @@
-import { Tabs } from "@kobalte/core/tabs"
 import { SettingsSection } from "./settings-section"
-import { PluginSettingsSection } from "./plugin-settings-section"
 
 export default function () {
   return (
     <div data-page="workspace-[id]">
       <div data-slot="sections">
-        <Tabs defaultValue="general" data-component="tabs">
-          <Tabs.List data-slot="tablist">
-            <Tabs.Trigger value="general" data-slot="tab">General</Tabs.Trigger>
-            <Tabs.Trigger value="plugins" data-slot="tab">Plugins</Tabs.Trigger>
-          </Tabs.List>
-          <Tabs.Content value="general">
-            <SettingsSection />
-          </Tabs.Content>
-          <Tabs.Content value="plugins">
-            <PluginSettingsSection />
-          </Tabs.Content>
-        </Tabs>
+        <SettingsSection />
       </div>
     </div>
   )
