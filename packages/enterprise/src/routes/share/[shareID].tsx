e03d62f99ac65282ad39e4194599916487447e51
Frank <97429702+tsubasakong@users.noreply.github.com>
2026-03-08 17:39:05 -0700

    fix(share): disable caching for live shared sessions

--- packages/enterprise/src/routes/share/[shareID].tsx
+++ packages/enterprise/src/routes/share/[shareID].tsx
@@ -108,10 +108,7 @@ const getData = query(async (shareID) => {
 }, "getShareData")
 
 export default function () {
-  getRequestEvent()?.response.headers.set(
-    "Cache-Control",
-    "public, max-age=30, s-maxage=300, stale-while-revalidate=86400",
-  )
+  getRequestEvent()?.response.headers.set("Cache-Control", "no-store")
 
   const params = useParams()
   const data = createAsync(async () => {
