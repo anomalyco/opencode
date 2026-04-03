ab9f482fab1f7b12ea36988b16f473d2ef7eda18
OpeOginni <brightoginni123@gmail.com>
2026-04-03 14:19:23 +0100

    fix: added back conditional system message for openau oauth models following https://github.com/anomalyco/opencode/blob/dev/packages/opencode/src/session/llm.ts#L149

--- packages/opencode/src/agent/agent.ts
+++ packages/opencode/src/agent/agent.ts
@@ -341,6 +341,16 @@ export namespace Agent {
           )
           const existing = yield* InstanceState.useEffect(state, (s) => s.list())
 
+          // TODO: clean this up so provider specific logic doesnt bleed over
+          const authInfo = yield* auth.get(model.providerID).pipe(Effect.orDie)
+
+          const isOpenaiOauth = model.providerID === "openai" && authInfo?.type === "oauth"
+
+          const USER_MESSAGE_CONTENT = {
+            role: "user",
+            content: `Create an agent configuration based on this request: \"${input.description}\".\n\nIMPORTANT: The following identifiers already exist and must NOT be used: ${existing.map((i) => i.name).join(", ")}\n  Return ONLY the JSON object, no other text, do not wrap in backticks`,
+          } as ModelMessage
+
           const params = {
             experimental_telemetry: {
               isEnabled: cfg.experimental?.openTelemetry,
@@ -349,18 +359,17 @@ export namespace Agent {
               },
             },
             temperature: 0.3,
-            messages: [
-              ...system.map(
-                (item): ModelMessage => ({
-                  role: "system",
-                  content: item,
-                }),
-              ),
-              {
-                role: "user",
-                content: `Create an agent configuration based on this request: \"${input.description}\".\n\nIMPORTANT: The following identifiers already exist and must NOT be used: ${existing.map((i) => i.name).join(", ")}\n  Return ONLY the JSON object, no other text, do not wrap in backticks`,
-              },
-            ],
+            messages: isOpenaiOauth
+              ? [USER_MESSAGE_CONTENT]
+              : [
+                  ...system.map(
+                    (item): ModelMessage => ({
+                      role: "system",
+                      content: item,
+                    }),
+                  ),
+                  USER_MESSAGE_CONTENT,
+                ],
             model: language,
             schema: z.object({
               identifier: z.string(),
@@ -369,8 +378,6 @@ export namespace Agent {
             }),
           } satisfies Parameters<typeof generateObject>[0]
 
-          // TODO: clean this up so provider specific logic doesnt bleed over
-          const authInfo = yield* auth.get(model.providerID).pipe(Effect.orDie)
           if (model.providerID === "openai" && authInfo?.type === "oauth") {
             return yield* Effect.promise(async () => {
               const result = streamObject({
