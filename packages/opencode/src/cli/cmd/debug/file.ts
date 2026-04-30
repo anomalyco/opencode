EldarBorge/opencode-local

feat(file): add fff-backed search

packages/opencode/src/cli/cmd/debug/file.ts
@@ -2,7 +2,7 @@ import { EOL } from "os"
 import { File } from "../../../file"
 import { bootstrap } from "../../bootstrap"
 import { cmd } from "../cmd"
-import { Ripgrep } from "@/file/ripgrep"
+import { Fff } from "@/file/fff"
 
 const FileSearchCommand = cmd({
   command: "search <query>",
@@ -77,7 +77,7 @@ const FileTreeCommand = cmd({
       default: process.cwd(),
     }),
   async handler(args) {
-    const files = await Ripgrep.tree({ cwd: args.dir, limit: 200 })
+    const files = await Fff.tree({ cwd: args.dir, limit: 200 })
     console.log(JSON.stringify(files, null, 2))
   },
 })

anomalyco/opencode

packages/opencode/src/cli/cmd/debug/file.ts
@@ -1,7 +1,7 @@
 import { EOL } from "os"
 import { AppRuntime } from "@/effect/app-runtime"
 import { File } from "../../../file"
-import { Ripgrep } from "@/file/ripgrep"
+import { Fff } from "@/file/fff"
 import { bootstrap } from "../../bootstrap"
 import { cmd } from "../cmd"
 
@@ -79,7 +79,7 @@ const FileTreeCommand = cmd({
     }),
   async handler(args) {
     await bootstrap(process.cwd(), async () => {
-      const tree = await AppRuntime.runPromise(Ripgrep.Service.use((svc) => svc.tree({ cwd: args.dir, limit: 200 })))
+      const tree = await Fff.tree({ cwd: args.dir, limit: 200 })
       console.log(JSON.stringify(tree, null, 2))
     })
   },