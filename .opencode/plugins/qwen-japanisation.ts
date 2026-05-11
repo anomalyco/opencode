e1d22796f39c652425b885090713c32229cc6347
Makochaaan <arowana326.315@gmail.com>
2026-05-11 14:38:27 +0900

    fix: polish LANGUAGE INSTRUCTION

--- .opencode/plugins/qwen-japanisation.ts
+++ .opencode/plugins/qwen-japanisation.ts
@@ -5,7 +5,7 @@ export const QwenJapanisationPlugin: Plugin = async () => {
     "experimental.chat.system.transform": async (input, output) => {
       if (input.model.providerID !== "qwen") {
         const LANGUAGE_INSTRUCTION = `
-          IMPORTANT: You response in the SAME LANGUAGE what the user use. If the user asks you in JAPANESE, you MUST use Japanese. And if the user asks you in ENGLISH, you MUST use English. NEVER mix languages in the conversation.
+          IMPORTANT: You respond in the SAME LANGUAGE which the user uses. If the user asks you in JAPANESE, you MUST use Japanese. And if the user asks you in ENGLISH, you MUST use English. NEVER mix some languages in the same conversation.
         `.trim()
         if (output.system[0]) {
           output.system[0] = `${LANGUAGE_INSTRUCTION}\n\n${output.system[0]}`
