75e3d69e5449ab63023c49a53aae21f84f441393
Makochaaan <arowana326.315@gmail.com>
2026-05-11 14:36:40 +0900

    fix: rename constant name and restrict only when they use qwen

--- .opencode/plugins/qwen-japanisation.ts
+++ .opencode/plugins/qwen-japanisation.ts
@@ -1,15 +1,18 @@
 import type { Plugin } from "@opencode-ai/plugin"
 
-export const MyCustomPromptPlugin: Plugin = async () => {
+export const QwenJapanisationPlugin: Plugin = async () => {
   return {
     "experimental.chat.system.transform": async (input, output) => {
-      const myCustomInstruction = `
-        IMPORTANT: You response in the SAME LANGUAGE what the user use. If the user asks you in JAPANESE, you MUST use Japanese. And if the user asks you in ENGLISH, you MUST use English. NEVER mix languages in the conversation.
-      `.trim()
-      if (output.system[0]) {
-        output.system[0] = `${myCustomInstruction}\n\n${output.system[0]}`
-        return
+      if (input.model.providerID !== "qwen") {
+        const LANGUAGE_INSTRUCTION = `
+          IMPORTANT: You response in the SAME LANGUAGE what the user use. If the user asks you in JAPANESE, you MUST use Japanese. And if the user asks you in ENGLISH, you MUST use English. NEVER mix languages in the conversation.
+        `.trim()
+        if (output.system[0]) {
+          output.system[0] = `${LANGUAGE_INSTRUCTION}\n\n${output.system[0]}`
+          return
+        }
       }
+      
     },
   }
 }
