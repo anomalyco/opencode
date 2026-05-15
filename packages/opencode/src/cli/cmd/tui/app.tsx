ee435104b6b7bd0f2646cf32e097b8bc098e7a78
Arjithar8 <arjithar8@gmail.com>
2026-05-16 00:02:39 +0530

    fix(opencode): use valid continue placeholder session id, since validation will fail and user will be shown an "unexpected server error" message instead of say Session not found which is raised further down the line when validation doesn't fail.

--- packages/opencode/src/cli/cmd/tui/app.tsx
+++ packages/opencode/src/cli/cmd/tui/app.tsx
@@ -209,7 +209,7 @@ export function tui(input: {
                         input.args.continue
                           ? {
                               type: "session",
-                              sessionID: "dummy",
+                              sessionID: "ses_dummy",
                             }
                           : undefined
                       }
