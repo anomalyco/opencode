ae33401ea4f9983ea95c7bc5014d53ac4db95f7a
neriousy <hejmowskifilip@gmail.com>
2026-05-18 15:39:37 +0200

    fix: desktop build script

--- packages/desktop/scripts/prebuild.ts
+++ packages/desktop/scripts/prebuild.ts
@@ -4,6 +4,8 @@ import { $ } from "bun"
 import { resolveChannel } from "./utils"
 
 const channel = resolveChannel()
+Bun.env.OPENCODE_CHANNEL = channel
+
 await $`bun ./scripts/copy-icons.ts ${channel}`
 await $`bun ./scripts/copy-metainfo.ts ${channel}`
 
