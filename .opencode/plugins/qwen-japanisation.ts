d399cac3271d15bbc3d56ac27276ea684ebe9838
sakamoto-souta <129728798+sakamoto-souta@users.noreply.github.com>
2026-05-18 21:21:36 +0900

    Update .opencode/plugins/qwen-japanisation.ts

--- .opencode/plugins/qwen-japanisation.ts
+++ .opencode/plugins/qwen-japanisation.ts
@@ -3,7 +3,7 @@ import type { Plugin } from "@opencode-ai/plugin"
 export const QwenJapanisationPlugin: Plugin = async () => {
   return {
     "experimental.chat.system.transform": async (input, output) => {
-      if (input.model.providerID !== "qwen") {
+      if (input.model.providerID === "qwen") {
         const LANGUAGE_INSTRUCTION = `
           IMPORTANT: You respond in the SAME LANGUAGE which the user uses. If the user asks you in JAPANESE, you MUST use Japanese. And if the user asks you in ENGLISH, you MUST use English. NEVER mix some languages in the same conversation.
         `.trim()
