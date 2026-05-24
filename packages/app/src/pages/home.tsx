df9958dd78b788e2d19eb84ac1bf780bd3b757df
Aarav Sareen <96787824+arvsrn@users.noreply.github.com>
2026-05-24 23:54:23 +0530

    improve projects sidebar

--- packages/app/src/pages/home.tsx
+++ packages/app/src/pages/home.tsx
@@ -301,7 +301,7 @@ function HomeProjectColumn(props: {
   const projects = createMemo(() => layout.projects.list())
   return (
     <aside class="flex min-w-0 flex-col lg:pt-[52px] mt-14" aria-label={props.language.t("home.projects")}>
-      <div class="flex h-7 min-w-0 items-center justify-between pl-3">
+      <div class="flex h-7 min-w-0 items-center justify-between pl-1.5">
         <div class={HOME_SECTION_LABEL}>{props.language.t("home.projects")}</div>
         <IconButtonV2
           data-action="home-add-project"
