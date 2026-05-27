512654252325efdc061d5a21593900b28434ebb9
Yuki Kitaya <72787572+yeighta@users.noreply.github.com>
2026-05-27 21:26:05 +0900

    Revert "feat(demo): ウェイティングリストを Google Form 連携で実装" (#240)

--- demo/app/page.tsx
+++ demo/app/page.tsx
@@ -1,7 +1,6 @@
 import { ScrollProgress } from "@/components/ScrollProgress"
 import { TopNav } from "@/components/TopNav"
 import { Hero } from "@/components/Hero"
-import { Waitlist } from "@/components/Waitlist"
 import { SectionProblem } from "@/components/SectionProblem"
 import { SectionTEE } from "@/components/SectionTEE"
 import { SectionHarness } from "@/components/SectionHarness"
@@ -17,15 +16,11 @@ export default function Page() {
       <TopNav />
       <main className="relative z-10">
         <Hero />
-        <Waitlist id="waitlist" />
         <SectionProblem />
         <SectionTEE />
-        <Waitlist variant="inline" />
         <SectionHarness />
         <SectionCoding />
-        <Waitlist variant="inline" />
         <SectionComparison />
-        <Waitlist />
         <CTA />
       </main>
       <Footer />
