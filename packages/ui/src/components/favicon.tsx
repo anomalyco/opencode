d545b1f86cac775b50481f479953bfa2fd1d56a2
Chad Voegele <chad@voegele.me>
2026-06-02 07:26:56 -0500

    Add credentials to manifest links

--- packages/ui/src/components/favicon.tsx
+++ packages/ui/src/components/favicon.tsx
@@ -6,7 +6,7 @@ export const Favicon = () => {
       <Link rel="icon" type="image/png" href="/favicon-96x96-v3.png" sizes="96x96" />
       <Link rel="shortcut icon" href="/favicon-v3.ico" />
       <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-v3.png" />
-      <Link rel="manifest" href="/site.webmanifest" />
+      <Link rel="manifest" href="/site.webmanifest" crossorigin="use-credentials" />
       <Meta name="apple-mobile-web-app-title" content="OpenCode" />
     </>
   )
