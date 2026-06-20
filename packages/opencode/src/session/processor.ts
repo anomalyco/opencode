b6e606bc500fc03abb45bf08e80b34eeab978fc7
affanali2k3 <affanalikhanxx@gmail.com>
2026-06-20 23:40:13 +0500

    fix formatting

--- packages/opencode/src/session/processor.ts
+++ packages/opencode/src/session/processor.ts
@@ -89,7 +89,7 @@ interface ProcessorContext extends Input {
 
 type StreamEvent = LLMEvent
 
-export class Service extends Context.Service<Service, Interface>()("@opencode/SessionProcessor") { }
+export class Service extends Context.Service<Service, Interface>()("@opencode/SessionProcessor") {}
 
 export const layer = Layer.effect(
   Service,
