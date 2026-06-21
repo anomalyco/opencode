hudawei996/opencode-main

feat(reload): add slash-driven config reload flow

packages/sdk/js/src/v2/gen/types.gen.ts
@@ -37,6 +37,7 @@ export type EventServerInstanceDisposed = {
   type: "server.instance.disposed"
   properties: {
     directory: string
+    reason?: string
   }
 }
 
@@ -53,7 +54,6 @@ export type EventInstallationUpdateAvailable = {
     version: string
   }
 }
-
 export type EventServerConnected = {
   type: "server.connected"
   properties: {
@@ -357,6 +357,28 @@ export type EventSessionCompacted = {
   type: "session.compacted"
   properties: {
     sessionID: string
+  }
+}
+
+export type EventConfigReloadPending = {
+  type: "config.reload.pending"
+  properties: {
+    pending: boolean
+  }
+}
+
+export type EventConfigReloadExecuting = {
+  type: "config.reload.executing"
+  properties: {
+    executing: boolean
+    bootstrapCycle?: number
+  }
+}
+
+export type EventConfigReloadDone = {
+  type: "config.reload.done"
+  properties: {
+    [key: string]: unknown
   }
 }
 
@@ -1115,6 +1137,9 @@ export type GlobalEvent = {
     | EventSessionStatus
     | EventSessionIdle
     | EventSessionCompacted
+    | EventConfigReloadPending
+    | EventConfigReloadExecuting
+    | EventConfigReloadDone
     | EventTuiPromptAppend
     | EventTuiCommandExecute
     | EventTuiToastShow
@@ -2027,6 +2052,9 @@ export type Event =
   | EventSessionStatus
   | EventSessionIdle
   | EventSessionCompacted
+  | EventConfigReloadPending
+  | EventConfigReloadExecuting
+  | EventConfigReloadDone
   | EventTuiPromptAppend
   | EventTuiCommandExecute
   | EventTuiToastShow
@@ -2806,6 +2834,28 @@ export type ExperimentalConsoleSwitchOrgResponses = {
 
 export type ExperimentalConsoleSwitchOrgResponse =
   ExperimentalConsoleSwitchOrgResponses[keyof ExperimentalConsoleSwitchOrgResponses]
+
+export type ConfigReloadData = {
+  body?: never
+  path?: never
+  query?: {
+    directory?: string
+    workspace?: string
+  }
+  url: "/config/reload"
+}
+
+export type ConfigReloadResponses = {
+  /**
+   * Configuration reloaded successfully
+   */
+  200: {
+    success: boolean
+    immediate: boolean
+  }
+}
+
+export type ConfigReloadResponse = ConfigReloadResponses[keyof ConfigReloadResponses]
 
 export type ToolIdsData = {
   body?: never

anomalyco/opencode

packages/sdk/js/src/v2/gen/types.gen.ts
@@ -77,13 +77,16 @@ export type Event =
   | EventMcpBrowserOpenFailed
   | EventCommandExecuted
   | EventProjectUpdated
+  | EventVcsBranchUpdated
+  | EventConfigReloadPending
+  | EventConfigReloadExecuting
+  | EventConfigReloadDone
   | EventSessionStatus
   | EventSessionIdle
   | EventQuestionAsked
   | EventQuestionReplied
   | EventQuestionRejected
   | EventSessionCompacted
-  | EventVcsBranchUpdated
   | EventWorkspaceReady
   | EventWorkspaceFailed
   | EventWorkspaceStatus
@@ -1516,6 +1519,35 @@ export type GlobalEvent = {
       }
     | {
         id: string
+        type: "vcs.branch.updated"
+        properties: {
+          branch?: string
+        }
+      }
+    | {
+        id: string
+        type: "config.reload.pending"
+        properties: {
+          pending: boolean
+        }
+      }
+    | {
+        id: string
+        type: "config.reload.executing"
+        properties: {
+          executing: boolean
+          bootstrapCycle?: number | "NaN" | "Infinity" | "-Infinity" | "Infinity" | "-Infinity" | "NaN"
+        }
+      }
+    | {
+        id: string
+        type: "config.reload.done"
+        properties: {
+          resumeSessionID?: string
+        }
+      }
+    | {
+        id: string
         type: "session.status"
         properties: {
           sessionID: string
@@ -1564,13 +1596,6 @@ export type GlobalEvent = {
         type: "session.compacted"
         properties: {
           sessionID: string
-        }
-      }
-    | {
-        id: string
-        type: "vcs.branch.updated"
-        properties: {
-          branch?: string
         }
       }
     | {
@@ -5021,6 +5046,39 @@ export type EventProjectUpdated = {
   }
 }
 
+export type EventVcsBranchUpdated = {
+  id: string
+  type: "vcs.branch.updated"
+  properties: {
+    branch?: string
+  }
+}
+
+export type EventConfigReloadPending = {
+  id: string
+  type: "config.reload.pending"
+  properties: {
+    pending: boolean
+  }
+}
+
+export type EventConfigReloadExecuting = {
+  id: string
+  type: "config.reload.executing"
+  properties: {
+    executing: boolean
+    bootstrapCycle?: number | "NaN" | "Infinity" | "-Infinity"
+  }
+}
+
+export type EventConfigReloadDone = {
+  id: string
+  type: "config.reload.done"
+  properties: {
+    resumeSessionID?: string
+  }
+}
+
 export type EventSessionStatus = {
   id: string
   type: "session.status"
@@ -5076,14 +5134,6 @@ export type EventSessionCompacted = {
   type: "session.compacted"
   properties: {
     sessionID: string
-  }
-}
-
-export type EventVcsBranchUpdated = {
-  id: string
-  type: "vcs.branch.updated"
-  properties: {
-    branch?: string
   }
 }
 
@@ -5552,6 +5602,68 @@ export type ConfigProvidersResponses = {
 }
 
 export type ConfigProvidersResponse = ConfigProvidersResponses[keyof ConfigProvidersResponses]
+
+export type ConfigReloadData = {
+  body?: never
+  path?: never
+  query?: {
+    directory?: string
+    workspace?: string
+  }
+  url: "/config/reload"
+}
+
+export type ConfigReloadErrors = {
+  /**
+   * Bad request
+   */
+  400: BadRequestError
+}
+
+export type ConfigReloadError = ConfigReloadErrors[keyof ConfigReloadErrors]
+
+export type ConfigReloadResponses = {
+  /**
+   * Configuration reload request result
+   */
+  200: {
+    success: boolean
+    immediate: boolean
+  }
+}
+
+export type ConfigReloadResponse = ConfigReloadResponses[keyof ConfigReloadResponses]
+
+export type ConfigBootstrapCompleteData = {
+  body?: never
+  path?: never
+  query?: {
+    directory?: string
+    workspace?: string
+    cycle?: string
+  }
+  url: "/config/bootstrap-complete"
+}
+
+export type ConfigBootstrapCompleteErrors = {
+  /**
+   * Bad request
+   */
+  400: BadRequestError
+}
+
+export type ConfigBootstrapCompleteError = ConfigBootstrapCompleteErrors[keyof ConfigBootstrapCompleteErrors]
+
+export type ConfigBootstrapCompleteResponses = {
+  /**
+   * Bootstrap completion result
+   */
+  200: {
+    success: boolean
+  }
+}
+
+export type ConfigBootstrapCompleteResponse = ConfigBootstrapCompleteResponses[keyof ConfigBootstrapCompleteResponses]
 
 export type ExperimentalCapabilitiesGetData = {
   body?: never