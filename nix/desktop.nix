985dd95256f4c359e0db2ba83e38d59a798e5a6c
Test User <test@test.com>
2026-07-13 23:56:54 +0200

    chore(nix): place postPatch comment before string

--- nix/desktop.nix
+++ nix/desktop.nix
@@ -42,8 +42,8 @@ stdenv.mkDerivation (finalAttrs: {
     ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
   };
 
-  # NOTE: Relax Bun version check to be a warning instead of an error
   postPatch =
+    # NOTE: Relax Bun version check to be a warning instead of an error
     ''
       substituteInPlace packages/script/src/index.ts \
         --replace-fail 'throw new Error(`This script requires bun@''${expectedBunVersionRange}' \
