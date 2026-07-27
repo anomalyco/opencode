920f3844b16e223da97df9266ead9c67a1cf5187
paolodelia99 <paolo.delia99@gmail.com>
2026-07-27 16:28:41 +0200

    feat(cron): register V2 cron tools in builtins

--- packages/core/src/cron/node.ts
+++ packages/core/src/cron/node.ts
@@ -1,5 +1,6 @@
 export * as CronNode from "./node"
 
+import type { LocationNode } from "../effect/app-node"
 import { Layer } from "effect"
 import { makeLocationNode } from "../effect/app-node"
 import { CronService, layer as cronLayer } from "./service"
@@ -9,7 +10,7 @@ import { SessionV2 } from "../session"
 import { Database } from "../database/database"
 import { EventV2 } from "../event"
 
-export const node = makeLocationNode({
+export const node: LocationNode<CronService> = makeLocationNode({
   service: CronService,
   layer: cronLayer.pipe(Layer.provide(CronDeliveryPortLive)),
   deps: [SessionExecution.node, SessionV2.node, Database.node, EventV2.node],
