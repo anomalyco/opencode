cce13a8886384089d4bb8db32ab34df302f501f1
Frederik Loukes <frederik.loukes@nsgo.nl>
2026-08-03 11:54:35 +0200

    fix: do not pass reasoning effort on gpt5.5+ with azure

--- packages/opencode/src/provider/transform.ts
+++ packages/opencode/src/provider/transform.ts
@@ -1270,7 +1270,11 @@ export function options(input: {
     result["gateway"] = { caching: "auto" }
   }
 
-  if (input.model.api.npm === "@ai-sdk/azure" && input.model.api.id.includes("gpt-5.5")) {
+  // Any gpt version above 5.4 in combination with azure does not support reasoningEffort
+  // so we should return early here.
+  const [, gptMajorVersion, gptMinorVersion] = input.model.api.id.match(/gpt-(\d+)\.(\d+)/) ?? []
+  const supported = Number(gptMajorVersion) > 5 || (Number(gptMajorVersion) === 5 && Number(gptMinorVersion) >= 5)
+  if (input.model.api.npm === "@ai-sdk/azure" && supported) {
     result["reasoningSummary"] = "auto"
     return result
   }
