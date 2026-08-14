fea2232aaf442b3e7e169a6f6d29c7f2ff58cde5
Kit Langton <kit.langton@gmail.com>
2026-08-13 20:57:34 -0400

    chore: remove orphaned v2 exports

--- packages/schema/src/durable-event-manifest.ts
+++ packages/schema/src/durable-event-manifest.ts
@@ -4,9 +4,4 @@ import { Event } from "./event.js"
 import { Worktree } from "./worktree.js"
 import { SessionEvent } from "./session-event.js"
 
-export const SessionDurable = {
-  definitions: Event.durableMap(SessionEvent.DurableDefinitions),
-  schema: SessionEvent.Durable,
-} as const
-
 export const Durable = Event.durableMap([...SessionEvent.DurableDefinitions, Worktree.Event.Resolved])
