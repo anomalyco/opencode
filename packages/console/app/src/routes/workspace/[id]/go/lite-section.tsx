aa0d66f4f8a4fbc467a4651c60dffa99fbe22329
Dax Raad <d@ironbay.co>
2026-08-25 11:15:44 -0400

    fix(console): rate limit checkout session creation

--- packages/console/app/src/routes/workspace/[id]/go/lite-section.tsx
+++ packages/console/app/src/routes/workspace/[id]/go/lite-section.tsx
@@ -20,6 +20,7 @@ import { formatResetTime, liteResetTimeKeys } from "~/lib/format-reset-time"
 import { createReferralFromCookie } from "~/lib/referral-invite"
 import { getRequestEvent } from "solid-js/web"
 import { countryFromRequest } from "~/lib/request-country"
+import { checkCheckoutRateLimit } from "~/routes/zen/util/redis"
 
 import { IconAlipay, IconUpi } from "~/component/icon"
 
@@ -86,6 +87,7 @@ const createLiteCheckoutUrl = action(
     "use server"
     return json(
       await withActor(async () => {
+        await checkCheckoutRateLimit(workspaceID)
         const data = await Billing.generateLiteCheckoutUrl({ successUrl, cancelUrl, method })
         await createReferralFromCookie()
         return { error: undefined, data }
