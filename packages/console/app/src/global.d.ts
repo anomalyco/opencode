aaa042f824143e9e5df0a8f3ea288aac6ab9b5eb
Kit Langton <kit.langton@gmail.com>
2026-08-28 14:20:37 -0400

    fix(console): restore request event locals typing

--- packages/console/app/src/global.d.ts
+++ packages/console/app/src/global.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="@solidjs/start/env" />
 
+import "solid-js/web"
+
+declare module "solid-js/web" {
+  interface RequestEvent {
+    locals: App.RequestEventLocals
+  }
+}
+
 export declare module "@solidjs/start/server" {
   export type APIEvent = { request: Request }
 }
