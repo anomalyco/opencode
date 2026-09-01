c166906f969a0c7e62fe3261994f2ccecc296b60
Michael Blakeley <mike+github@blakeley.com>
2026-09-01 15:52:18 -0700

    feat(plugin): expose session forms, session list, permissions, skills, compact, revert and global event stream

--- packages/core/test/worktree.test.ts
+++ packages/core/test/worktree.test.ts
@@ -912,7 +912,7 @@ describe("Worktree", () => {
       const git = yield* WorktreeGit.make
       yield* worktrees.transform((editor) => editor.add({ ...git, id: Worktree.StrategyID.make("custom") }))
       yield* ConfigWorktreePlugin.Plugin.effect(
-        host({ event: { subscribe: () => bus.subscribe().pipe(Stream.filter(EventManifest.isServer)) } }),
+        host({ event: { subscribe: () => bus.subscribe().pipe(Stream.filter(EventManifest.isServer)), subscribeGlobal: () => Stream.empty } }),
       ).pipe(
         Effect.provideService(Worktree.Service, {
           ...worktrees,
