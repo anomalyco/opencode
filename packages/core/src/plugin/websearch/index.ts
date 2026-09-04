7c7cdb97cee09bdaa604253bd46edbedae593f9a
Ilya Bogin <ilya.bogin@keenable.ai>
2026-09-04 12:02:07 +0300

    feat(core): add keenable web search

--- packages/core/src/plugin/websearch/index.ts
+++ packages/core/src/plugin/websearch/index.ts
@@ -1,5 +1,6 @@
 import { WebSearchExa } from "./exa.js"
 import { WebSearchFirecrawl } from "./firecrawl.js"
+import { WebSearchKeenable } from "./keenable.js"
 import { WebSearchParallel } from "./parallel.js"
 import { WebSearchTavily } from "./tavily.js"
 import { WebSearchTinyFish } from "./tinyfish.js"
@@ -7,6 +8,7 @@ import { WebSearchTinyFish } from "./tinyfish.js"
 export const WebSearchPlugins = [
   WebSearchExa.Plugin,
   WebSearchFirecrawl.Plugin,
+  WebSearchKeenable.Plugin,
   WebSearchParallel.Plugin,
   WebSearchTavily.Plugin,
   WebSearchTinyFish.Plugin,
